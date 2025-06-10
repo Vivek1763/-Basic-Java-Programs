b65f95aedc0831104669f9d248b2171b4fdf0b90
Vivek Deshmukh <vivekdd1005@gmail.com>
2025-06-10 23:10:33 +0530

    Add files via upload

---
+++ palidrome.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+class Palidrome {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a number:");
+        int num = input.nextInt();
+        int original = num;
+        int reverse = 0;
+
+        while (num != 0) {
+            int digit = num % 10;
+            reverse = reverse * 10 + digit;
+            num /= 10;
+        }
+
+        if (original == reverse)
+            System.out.println(original + " is a Palindrome");
+        else
+            System.out.println(original + " is not a Palindrome");
+    }
+}
