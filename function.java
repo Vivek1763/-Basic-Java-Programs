b764fb1c8a41490b694b667e455c36e913ebaf7f
Vivek Deshmukh <vivekdd1005@gmail.com>
2025-06-10 23:15:58 +0530

    Add files via upload

---
+++ function.java
@@ -0,0 +1,9 @@
+public class function {
+    public static void myClass(String Name) {
+        System.out.println(Name);
+
+    }
+    public static void main(String[] args) {
+        myClass("Vivek");
+    }
+}
