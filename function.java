835ec38ce011b481f6eb1fa0685bbc10cd3f0243
Vivek Deshmukh <vivekdd1005@gmail.com>
2025-06-13 08:45:10 +0530

    Delete function.java

--- function.java
+++
@@ -1,9 +0,0 @@
-public class function {
-    public static void myClass(String Name) {
-        System.out.println(Name);
-
-    }
-    public static void main(String[] args) {
-        myClass("Vivek");
-    }
-}
